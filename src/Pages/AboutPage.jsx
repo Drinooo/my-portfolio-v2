d85ecb1d959339a466797efcaf30c797b79c6a51
NMS Design <nmsdesign@AldrinITD.local>
2023-06-23 16:17:24 +0800

    Initial Commit

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -1,10 +1,10 @@
 import { Box, Typography } from "@mui/material";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { WIPIcon } from "../MuiComponents/Icons";
 import { WIP } from "../Sections/WIP";
 
 export const AboutPage = () => {
   return (
-    <WIP />
+      <WIP />
   );
 };

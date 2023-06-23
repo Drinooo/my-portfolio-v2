afd3c608134cc0c2b3839128df0f6f895b121e3c
NMS Design <nmsdesign@AldrinITD.local>
2023-06-23 16:21:49 +0800

    Initial commit only

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,11 @@ import { Footer } from "./Sections/Footer";
 import { Superare } from "./Pages/Superare";
 import { Route, Routes } from "react-router-dom";
 import { MainRoutes } from "./MainRoutes";
+import { Home } from "./Pages/HomePage";
+import { AboutPage } from "./Pages/AboutPage";
+import { ProjectsPage } from "./Pages/ProjectsPage";
+import { SkillsPage } from "./Pages/SkillsPage";
+import { ContactPage } from "./Pages/ContactPage";
 
 const App = () => {
   return (
@@ -20,7 +25,13 @@ const App = () => {
         <CssBaseline />
         <Stack gap={"150px"}>
           <CustAppBar />
-          <MainRoutes />
+          <Routes>
+            <Route path="/Home" element={<Home />} />
+            <Route path="/About" element={<AboutPage />} />
+            <Route path="/Projects" element={<ProjectsPage />} />
+            <Route path="/Skills" element={<SkillsPage />} />
+            <Route path="/Contact" element={<ContactPage />} />
+          </Routes>
           <Footer />
         </Stack>
       </ThemeProvider>

a9f72fb271e5bd1d74e325eaf08f463e315744a3
NMS Design <nmsdesign@AldrinITD.local>
2023-06-23 16:03:17 +0800

    WIP Page Responsive

--- src/Sections/WIP.jsx
+++ src/Sections/WIP.jsx
@@ -1,16 +1,18 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 import React from "react";
 import { WIPIcon } from "../MuiComponents/Icons";
 
 export const WIP = () => {
   return (
     <Box textAlign="center">
-      <WIPIcon />
+      <Container maxWidth="md">
+        <WIPIcon />
+      </Container>
+
       <Box mt={5}>
         <Typography variant="h2">
           This page is currently in the development phase.
         </Typography>
-        <Typography variant="h4">-Dev Aldrin</Typography>
       </Box>
     </Box>
   );
